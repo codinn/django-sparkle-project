1ad83a25e4da21d03e3cad005396cdae3464aa26
vicalloy <zbirder@gmail.com>
2017-08-23 15:48:16 +0800

    Update urls.py

--- sparkle/urls.py
+++ sparkle/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from sparkle.views import appcast
 
-urlpatterns = (
+urlpatterns = [
     url(r'^(?P<application_id>\d+)/appcast.xml$', appcast, name='sparkle_application_appcast'),
-)
+]
