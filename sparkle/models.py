210c23074ff33b0a015d45934e64670df3e04760
vicalloy <zbirder@gmail.com>
2017-09-14 10:05:25 +0800

    update version info on save

--- sparkle/models.py
+++ sparkle/models.py
@@ -1,5 +1,12 @@
 import os
+import zipfile
+import tempfile
+import plistlib
+import shutil
+
 from django.db import models
+from django.conf import settings
+
 from sparkle.conf import SPARKLE_PRIVATE_KEY_PATH
 
 
@@ -25,25 +32,64 @@ class Version(models.Model):
     release_notes = models.TextField(blank=True, null=True)
     minimum_system_version = models.CharField(blank=True, null=True, max_length=10)
     published = models.DateTimeField(auto_now_add=True)
-    update = models.FileField()
+    update = models.FileField(upload_to='sparkle/')
     active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
+        path = os.path.join(settings.MEDIA_ROOT, self.update.path)
 
         # if there is no dsa signature and a private key is provided in the settings
         if not self.dsa_signature and SPARKLE_PRIVATE_KEY_PATH and os.path.exists(SPARKLE_PRIVATE_KEY_PATH):
-            command = 'openssl dgst -sha1 -binary < "%s" | openssl dgst -dss1 -sign "%s" | openssl enc -base64' % (path, SPARKLE_PRIVATE_KEY_PATH)
+            command = 'openssl dgst -sha1 -binary < "%s" | openssl dgst -dss1 -sign "%s" | openssl enc -base64' % (
+                path, SPARKLE_PRIVATE_KEY_PATH)
             process = os.popen(command)
             self.dsa_signature = process.readline().strip()
             process.close()
 
-        # Calculate file size
-        if not self.length:
-            # TODO: set correct size
-            self.length = 0
+        # if there is no length and it is a zip file
+        # extract it to a tempdir and calculate the length
+        # also parse the plist file for versions
+        if not self.length and path.endswith('.zip'):
+                zip_file = zipfile.ZipFile(path)
+                tempdir = tempfile.mkdtemp()
+                files = zip_file.namelist()
+                start_path = None
+
+                for f in files:
+                    if f.endswith('/'):
+                        d = os.path.join(tempdir, f)
+                        if not start_path:
+                            start_path = d
+                            os.makedirs(d)
+                    else:
+                        zip_file.extract(f, tempdir)
+
+                total_size = 0
+                for dirpath, dirnames, filenames in os.walk(start_path):
+                    for f in filenames:
+                        fp = os.path.join(dirpath, f)
+                        total_size += os.path.getsize(fp)
+
+                info_plist = os.path.join(start_path, 'Contents/Info.plist')
+
+                if os.path.exists(info_plist):
+                    plist = plistlib.readPlist(info_plist)
+
+                    if not self.version and 'CFBundleVersion' in plist:
+                        self.version = plist.get('CFBundleVersion')
+
+                    if not self.short_version and 'CFBundleShortVersionString' in plist:
+                        self.short_version = plist.get('CFBundleShortVersionString')
+
+                    if not self.minimum_system_version and 'LSMinimumSystemVersion' in plist:
+                        self.minimum_system_version = plist.get('LSMinimumSystemVersion')
+
+                shutil.rmtree(tempdir)
+
+                self.length = total_size
 
         super(Version, self).save(*args, **kwargs)
 
